1e4a94ada1bfa0a2c87a3e390fd0674346ae3997
letsouzars <leticia.souza17senai@gmail.com>
2023-08-28 11:04:50 -0300

    x

--- projetinhoss/main.js
+++ projetinhoss/main.js
@@ -7,9 +7,9 @@ let ganhador = -1 ;
 function jogar(escolha) {
     jogadorEscolha = escolha;
     computadorEscolha = Math.floor(Math.random() * (3 - 1 + 1)) +1;
-    // PEDRA = 1
-    // PAPEL = 2
-    // TESOURA = 3 
+    // PEDRA = 1     EU = 1
+    // PAPEL = 2     COMPUTADOR = 2
+    // TESOURA = 3    EMPATE = 0
 
     if (jogadorEscolha == 1 && computadorEscolha == 1) {
         ganhador = 0;
@@ -17,26 +17,30 @@ function jogar(escolha) {
         ganhador = 2;
     } else if (jogadorEscolha == 1 && computadorEscolha == 3){
         ganhador = 1;
-    }
-
-
-
-    if (jogadorEscolha == 2 && computadorEscolha == 1) {
-        ganhador = 2;
+    }else if (jogadorEscolha == 2 && computadorEscolha == 1) {
+        ganhador = 1;
     } else if (jogadorEscolha == 2 && computadorEscolha == 2) {
         ganhador = 0;
     } else if (jogadorEscolha == 2 && computadorEscolha == 3){
-        ganhador = 3;
-    }
-
-
-    if (jogadorEscolha == 3 && computadorEscolha == 1) {
-        ganhador = 1;
+        ganhador = 2;
+    } else if (jogadorEscolha == 3 && computadorEscolha == 1) {
+        ganhador = 2;
     } else if (jogadorEscolha == 3 && computadorEscolha == 2) {
-        ganhador = 3;
+        ganhador = 1;
     } else if (jogadorEscolha == 3 && computadorEscolha == 3){
         ganhador = 0;
     }
     
 
+    document.getElementById('jogadorEscolha1').classList.remove('selecionado;');
+    document.getElementById('jogadorEscolha2').classList.remove('selecionado;');
+    document.getElementById('jogadorEscolha3').classList.remove('selecionado;');
+
+    document.getElementById('computadorEscolha1').classList.remove('selecionado;');
+    document.getElementById('computadorEscolha2').classList.remove('selecionado;');
+    document.getElementById('computadorEscolha3').classList.remove('selecionado;');
+
+
+    document.getElementById('jogadorEscolha' + jogadorEscolha). classList.add('selecionado');
+    document.getElementById('computadorEscolha' + computadorEscolha). classList.add ('selecionado');
 }
